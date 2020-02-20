bbb1e65617d659cc97483b53eed9d80135b180bd
Leon <leon.cruz.teixeira@gmail.com>
2020-02-20 09:45:09 -0300

    Estilização inicial da home

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Olá, home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá, Ceci</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F3F3',
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 20,
+    position: 'absolute',
+    top: hp(2),
+  },
+});
