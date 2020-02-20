4939cadfa5975ce67e0de4a64cd3c28dfe8bc2a9
Leon <leon.cruz.teixeira@gmail.com>
2020-02-20 20:33:55 -0300

    Alteração no componente

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, FlatList, StyleSheet} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 import A from '../../assets/alphabet/a.svg';
 
-function Card() {
+function Card({item}) {
   return (
     <View style={styles.card}>
-      <A width={wp(25)} height={wp(25)} />
+      <A width={100} height={100} />
     </View>
   );
 }
 
 export default function CardList(props) {
+  const [letters, setLetters] = useState(
+    'a b c d e f g h i k j l m n o p q r s t u v w x y z'.split(' '),
+  );
+
   return (
     <View>
       <FlatList
         data={props.data}
-        renderItem={() => <Card />}
+        renderItem={item => Card(item)}
         keyExtractor={item => item}
         numColumns={2}
       />
