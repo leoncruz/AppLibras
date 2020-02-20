4ffabe9aafd7539f9a5f521cc16fccb5cd654597
Leon <leon.cruz.teixeira@gmail.com>
2020-02-20 00:03:23 -0300

    Adição do componente Card a tela

--- src/screens/NameLibrasScreen/index.js
+++ src/screens/NameLibrasScreen/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {Card, Button} from '../../compoents';
 
 export default function NameLibrasScreen() {
   return (
-    <View>
-      <Text>Olá, Nome em libras</Text>
+    <View style={styles.container}>
+      <View style={{position: 'absolute', top: hp(10)}}>
+        <Text style={{textAlign: 'center', fontSize: 20}}>Olá, Ceci</Text>
+        <Text style={{fontSize: 20}}>Seu nome em LIBRAS é assim:</Text>
+      </View>
+      <View style={styles.cards}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </View>
+      <Button title="Concluído" top={hp(5)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F3F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  cards: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+});
